36c5fa2feb2d2f66464edc67ea63f635ccc9c853
sabikunnahar16 <sabikunnahar789789@gmail.com>
2025-12-06 12:20:46 +0600

    Create caeser.py

---
+++ caeser.py
@@ -0,0 +1,38 @@
+def encrypt(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isalpha():  # only letters
+            base = ord('a')
+            # Shift and convert to uppercase
+            encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+            result += encrypted_char.upper()
+        else:
+            result += char  
+    return result
+
+
+def decrypt(cipher_text, shift):
+    result = ""
+
+    for char in cipher_text:
+        if char.isalpha():
+            base = ord('A')
+            # Shift backward and convert to lowercase
+            decrypted_char = chr((ord(char) - base - shift) % 26 + base)
+            result += decrypted_char.lower()
+        else:
+            result += char
+    return result
+
+
+
+plaintext = "hello world"
+shift_value = 3
+
+cipher = encrypt(plaintext, shift_value)
+plain = decrypt(cipher, shift_value)
+
+print("Plaintext :", plaintext)
+print("Encrypted :", cipher)
+print("Decrypted :", plain)
